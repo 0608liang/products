08f8a77eee6bc700af84432c5eab92c799ede880
0608liang <marc.cl.kuo@gmail.com>
2023-07-22 17:16:55 +0800

    new version

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True :
 	if name == 'q' :
 		break
 	price = input('請輸入產品價格: ')
+	price = int(price)
 	if price == 'q' :
 		break
 	p = [name, price]
@@ -15,3 +16,8 @@ print(products[0][0])
 
 for p in products :
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w') as f :
+	f.write('商品,價格\n')
+	for p in products :
+		f.write(p[0] + ',' + str(p[1]) + '\n')
